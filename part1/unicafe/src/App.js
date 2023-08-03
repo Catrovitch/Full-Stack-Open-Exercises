5e2e97e7df6e940d573adeb5c1f1b0ff5f672f80
Catrovitch <ale.engelhardt@outlook.com>
2023-08-03 15:03:31 +0300

    1.7: unicafe step2

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -13,11 +13,46 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
+  const [average, setAverage] = useState(0)
+  const [positive, setPositive] = useState(0)
 
-  const GoodFeedback = () => setGood(good +1)
-  const NeutralFeedback = () => setNeutral(neutral +1)
-  const BadFeedback = () => setBad(bad +1)
+  const GoodFeedback = () => {
+    setGood(good+1)
+    setAll(all+1)
+    const updatedGood = good +1
+    const updatedAll = all +1
+    setAverage((updatedGood-bad)/updatedAll)
+    const positiveFeedbackPercentage = PositivePercentage({good: updatedGood, all: updatedAll})
+    setPositive(positiveFeedbackPercentage)
 
+  }
+
+  const NeutralFeedback = () => {
+    setNeutral(neutral+1)
+    setAll(all+1)
+    const updatedAll = all +1
+    const positive_percentage = good/updatedAll
+    const positive_percentage_string = (positive_percentage.toString()+'%')
+    setPositive(positive_percentage_string)
+  }  
+  
+  const BadFeedback = () => {
+    setBad(bad+1)
+    setAll(all+1)
+    const updatedBad = bad+1
+    const updatedAll = all +1
+    setAverage((good-updatedBad)/updatedAll)
+    const positive_percentage = good/updatedAll
+    const positive_percentage_string = (positive_percentage.toString()+'%')
+    setPositive(positive_percentage_string)
+  }
+
+  const PositivePercentage = ({ good, all }) => {
+    const positive_percentage = (good/all)*100
+    const positive_percentage_string = (positive_percentage.toString()+'%')
+    return positive_percentage_string
+  }
 
   return (
     <div>
@@ -38,7 +73,9 @@ const App = () => {
       <Display text='good' value= {good}></Display>
       <Display text='neutral' value= {neutral}></Display>
       <Display text='bad' value= {bad}></Display>
-      
+      <Display text='all' value= {all}></Display>
+      <Display text='average' value= {average}></Display>
+      <Display text='positive' value= {positive} ></Display>
     </div>
   )
 }
