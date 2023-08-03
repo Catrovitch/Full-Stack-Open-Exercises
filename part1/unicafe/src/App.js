69e161110d530147110a532a77d9f0db9bc78688
Catrovitch <ale.engelhardt@outlook.com>
2023-08-03 16:09:32 +0300

    1.11: unicafe step6 + fix for earlier bugs

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -17,23 +17,23 @@ const Statistics = (props) => {
   if (all === 0) {
     return (
       <>
-        <Header text='statistics'/>
         <Display text='No feedback given'></Display>
       </>
     );
   }
-  
   return (
-    <>
-      <Header text='statistics'/>
-      <StatisticsLine text='good' value={good}/>
-      <StatisticsLine text='neutral' value={neutral}/>
-      <StatisticsLine text='bad' value={bad}/>
-      <StatisticsLine text='all' value={all}/>
-      <StatisticsLine text='average' value={average}/>
-      <StatisticsLine text='positive' value={positive}/>
-    </>
+    <table>
+      <tbody> 
+        <StatisticsLine text='good' value={good}/>
+        <StatisticsLine text='neutral' value={neutral}/>
+        <StatisticsLine text='bad' value={bad}/>
+        <StatisticsLine text='all' value={all}/>
+        <StatisticsLine text='average' value={average}/>
+        <StatisticsLine text='positive' value={positive}/>
+      </tbody>
+    </table>
   );
+  
 };
 
 
@@ -97,6 +97,7 @@ const App = () => {
         handleClick={BadFeedback}
         text='bad'
       />
+      <Header text='statistics'/>
       <Statistics 
         good={good}
         neutral={neutral}
