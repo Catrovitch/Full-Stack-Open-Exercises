48fbfd7d0abecc65327a145bc7c23652e95b4d64
Catrovitch <ale.engelhardt@outlook.com>
2023-08-08 11:45:22 +0300

    2.11: The Phonebook Step6

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,20 +1,31 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import Numbers from './components/Numbers'
-import FormField from './components/FormField'
 import AddNewPeopleForm from './components/AddNewPeopleForm'
 
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [searchName, setSearchName] = useState('')
   const [matchingNames, setMatchingNames] = useState(persons)
+
+  const hook = () => {
+    axios
+        .get('http://localhost:3001/persons')
+        .then(response => {
+          console.log('promise fulfilled')
+          console.log('Data', response.data)
+          setPersons(response.data)
+          setMatchingNames(response.data)
+        })
+  }
+
+  useEffect(hook, [])
+
+  console.log('render', persons.length, 'persons')
+
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
