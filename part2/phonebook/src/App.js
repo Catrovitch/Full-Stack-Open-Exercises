e938ec672cee9fd1ee44129043695ce9d46bf204
Catrovitch <ale.engelhardt@outlook.com>
2023-08-08 17:03:22 +0300

    2.13: The Phonebook step8

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -22,9 +22,6 @@ const App = () => {
 
   useEffect(hook, [])
 
-  console.log('render', persons.length, 'persons')
-
-
   const handleNameChange = (event) => {
     setNewName(event.target.value)
   }
@@ -40,30 +37,32 @@ const App = () => {
   const addPerson = (event) => {
     event.preventDefault();
     if (checkNames(newName)) {
-      alert(`${newName} is already added to the phonebook`);
+      alert(`${newName} already exists in the phonebook`);
     } else if (newName === '') {
       alert('Name field cannot be empty');
     } else {
-      const nameObject = {
-        name: newName,
-        number: newNumber
-      };
-      const updatedPersons = persons.concat(nameObject)
-      setPersons(updatedPersons);
-      console.log(updatedPersons)
-      const updatedMatchingNames = searchPersons(updatedPersons, searchName)
-      setMatchingNames(updatedMatchingNames)
-      setNewName('');
-      setNewNumber('');
+    const nameObject = {
+      name: newName,
+      number: newNumber
+    };
+      personService
+        .create(nameObject)
+        .then(returnedPerson => {
+          const updatedPersons = persons.concat(returnedPerson)
+          setPersons(updatedPersons)
+          const updatedMatchingNames = searchPersons(updatedPersons, searchName)
+          setMatchingNames(updatedMatchingNames)
+        })
     }
   };
-  
+
   const searchPersons = (updatedPersons, searchName) => {
     const filteredList = updatedPersons.filter((updatedPerson) =>
       updatedPerson.name.toLowerCase().includes(searchName.toLowerCase())
     );
     return filteredList;
-  };
+    };
+
   
 
   const checkNames = (name) => {
--- part2/phonebook/src/services/persons.js
+++ part2/phonebook/src/services/persons.js
@@ -7,5 +7,9 @@ const getAll = () => {
     return request.then(response => response.data)
 }
 
+const create = newObject => {
+    const request = axios.post(baseUrl, newObject)
+    return request.then(response => response.data)
+}
 
-export default { getAll }
+export default { getAll, create }
