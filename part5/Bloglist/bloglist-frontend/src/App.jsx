01592ef14aab065d7212e5fdcd5a005b06756a2f
Catrovitch <ale.engelhardt@outlook.com>
2023-08-29 10:53:12 +0300

    5.1, 5.2, 5.3 done

---
+++ part5/Bloglist/bloglist-frontend/src/App.jsx
@@ -0,0 +1,192 @@
+import { useState, useEffect } from 'react'
+import Blog from './components/Blog'
+import Notification from './components/Notification'
+import Footer from './components/Footer'
+import blogService from './services/blogs'
+import loginService from './services/login'
+
+const App = () => {
+  const [blogs, setBlogs] = useState([])
+  const [newBlogTitle, setNewBlogTitle] = useState('')
+  const [newBlogAuthor, setNewBlogAuthor] = useState('')
+  const [newBlogUrl, setNewBlogUrl] = useState('')
+  const [errorMessage, setErrorMessage] = useState(null)
+  const [username, setUsername] = useState('') 
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
+
+  useEffect(() => {
+    blogService
+      .getAll()
+      .then(initialBlogs => {
+        setBlogs(initialBlogs)
+      })
+  }, [])
+
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
+  }, [])
+
+  const addBlog = (event) => {
+    event.preventDefault()
+    const blogObject = {
+      content: newBlog,
+      important: Math.random() > 0.5,
+    }
+  
+    blogService
+      .create(blogObject)
+        .then(returnedBlog => {
+        setBlogs(blogs.concat(returnedBlog))
+        setNewBlog('')
+      })
+  }
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    
+    try {
+      const user = await loginService.login({
+        username, password,
+      })
+      window.localStorage.setItem(
+        'loggedBlogappUser', JSON.stringify(user)
+      ) 
+      blogService.setToken(user.token)
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      console.log(exception)
+      setErrorMessage('Wrong credentials')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    }
+  }
+
+  const loginForm = () => (
+    <form onSubmit={handleLogin}>
+      <div>
+        username
+          <input
+          type="text"
+          value={username}
+          name="Username"
+          onChange={({ target }) => setUsername(target.value)}
+        />
+      </div>
+      <div>
+        password
+          <input
+          type="password"
+          value={password}
+          name="Password"
+          onChange={({ target }) => setPassword(target.value)}
+        />
+      </div>
+      <button type="submit">login</button>
+    </form>      
+  )
+  
+  const loggedInUser = (user) => (
+    <div>
+      <p>{user.name} logged in</p>
+      <button type="submit" onClick={handleLogout}>logout</button>
+    </div>
+  )
+  const handleLogout = async (event) => {
+    event.preventDefault();
+  
+    try {
+      window.localStorage.removeItem('loggedBlogappUser');      blogService.setToken(user.token)
+      setUser('')
+      setUsername('')
+      setPassword('')
+    } catch (error) {
+      console.error('Error while logging out:', error);
+    }
+  };
+
+  const handleBlogTitleChange = (event) => {
+    setNewBlogTitle(event.target.value)
+  }
+
+
+  const handleBlogAuthorChange = (event) => {
+    setNewBlogAuthor(event.target.value)
+  }
+
+
+  const handleBlogUrlChange = (event) => {
+    setNewBlogUrl(event.target.value)
+  }
+  
+  const blogForm = () => (
+    <div>
+    <h2>Create new</h2>
+    <form onSubmit={addBlog}>
+      <div>
+        <label>Title:</label>
+        <input
+          type="text"
+          value={newBlogTitle}
+          onChange={handleBlogTitleChange}
+        />
+      </div>
+      <div>
+        <label>Author:</label>
+        <input
+          type="text"
+          value={newBlogAuthor}
+          onChange={handleBlogAuthorChange}
+        />
+      </div>
+      <div>
+        <label>URL:</label>
+        <input
+          type="text"
+          value={newBlogUrl}
+          onChange={handleBlogUrlChange}
+        />
+      </div>
+      <button type="submit">Create</button>
+    </form>
+    </div>
+  );
+  
+
+
+  return (
+    <div>
+      <h1>Bloglist</h1>
+
+      <Notification message={errorMessage} />
+
+      {!user ? (
+        loginForm()
+      ) : (
+        <div>
+          {loggedInUser(user)}
+          {blogForm()}
+        </div>
+      )}
+      <div>
+        <h2>blogs</h2>
+        <ul>
+          {blogs.map(blog => (
+            <Blog key={blog.id} blog={blog} />
+          ))}
+        </ul>
+        <Footer></Footer>
+      </div>
+    </div>
+  )
+}
+
+export default App
