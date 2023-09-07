751051a0ec3d29fed7f7a32eda99047c34b42e2d
Catrovitch <ale.engelhardt@outlook.com>
2023-09-07 14:37:22 +0300

    6.15 Anecdotes and the backend, step2

--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,24 +1,13 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 
-const getId = () => (100000 * Math.random()).toFixed(0);
-
-const asObject = (anecdote) => {
-  return {
-    content: anecdote,
-    id: getId(),
-    votes: 0
-  };
-};
-
 
 const anecdoteSlice = createSlice({
   name: 'anecdotes',
   initialState: [],
   reducers: {
     createAnecdote: (state, action) => {
-      const anecdote = asObject(action.payload)
-      state.push(anecdote)
+      state.push(action.payload)
     },
     vote: (state, action) => {
       const id = action.payload;
